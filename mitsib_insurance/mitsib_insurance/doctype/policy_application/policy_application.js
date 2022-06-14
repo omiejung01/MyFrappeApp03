9122b80833be4633deebb42925c3648feda76f65
Omie C <omiejung@gmail.com>
2022-06-14 01:14:10 +0000

    This is to record MITSIB

---
+++ mitsib_insurance/mitsib_insurance/doctype/policy_application/policy_application.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Dhirachat Chayaporn and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Policy Application', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ mitsib_insurance/mitsib_insurance/doctype/policy_application/test_policy_application.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Dhirachat Chayaporn and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestPolicyApplication(unittest.TestCase):
+	pass
